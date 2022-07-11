6d6e7b274a72a27a7d3b808419586a32190ecc4d
Júnior Moura <geraldomartins10@gmail.com>
2022-07-11 18:00:09 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
-function App() {
-    return(
-      <h1>Olá mundo!</h1>
-    )
-  }
+import NotificationButton from "./components/NotificationButton";
 
-export default App
+function App() {
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+    </>
+  );
+}
+
+export default App;
